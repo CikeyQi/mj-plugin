5174c6dc951e35246dde045f3b047ed6020f58fa
苏沫柒 <3146312184@qq.com>
2023-08-25 22:23:47 +0800

    Update Proxy.js

--- components/Proxy.js
+++ components/Proxy.js
@@ -1,17 +1,21 @@
 import fetch from "node-fetch";
 import { HttpsProxyAgent } from "https-proxy-agent";
+import Config from "../components/Config.js";
 
-async function getPic (imageUrl) {
-    const agent = new HttpsProxyAgent("http://127.0.0.1:7890", {
+async function getPic(imageUrl) {
+    let agent = null;
+    if ((Config.getConfig()).proxy) {
+      agent = new HttpsProxyAgent((Config.getConfig()).proxy_url, {
         keepAlive: true,
-    });
+      });
+    }
     const res = await fetch(imageUrl, {
-        agent: agent,
+      agent: agent,
     });
     const base64 = await res.arrayBuffer().then((buffer) => {
-        return Buffer.from(buffer).toString("base64");
+      return Buffer.from(buffer).toString("base64");
     });
     return base64;
-}
-
-export default getPic;
+  }
+  
+  export default getPic;

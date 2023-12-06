2f0ef1d6e22289837c2f459d5e3253d77016b18c
ZeroCal Su <3146312184@qq.com>
2023-12-06 08:24:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-import fs from 'node:fs'
-import Init from './model/init.js'
+import fs from 'node:fs';
+
 if (!global.segment) {
-  global.segment = (await import('oicq')).segment
+  global.segment = (await import("oicq")).segment;
 }
-const catlist = ['😸', '😹', '😺', '😻', '😼', '😽', '😾', '😿', '🙀']
-logger.info('---------------')
-logger.mark(
-  logger.green(
-    `[${
-      catlist[Math.floor(Math.random() * catlist.length)]
-    }]MJ-Plugin插件自检中......`
-  )
-)
-logger.mark(
-  logger.green(
-    'MJ插件交流群：璃月621069204，稻妻646582537，群内有官方绘图机器人'
-  )
-)
+
+let ret = [];
+
+logger.info(logger.yellow("- 正在载入 MJ-PLUGIN"));
+
 const files = fs
   .readdirSync('./plugins/mj-plugin/apps')
-  .filter((file) => file.endsWith('.js'))
-let ret = []
+  .filter((file) => file.endsWith('.js'));
+
 files.forEach((file) => {
   ret.push(import(`./apps/${file}`))
 })
-ret = await Promise.allSettled(ret)
-const apps = {}
-for (const i in files) {
-  const name = files[i].replace('.js', '')
 
-  if (ret[i].status != 'fulfilled') {
-    logger.error(`载入插件错误：${logger.red(name)}`)
-    logger.error(ret[i].reason)
-    continue
+ret = await Promise.allSettled(ret);
+
+let apps = {};
+for (let i in files) {
+  let name = files[i].replace('.js', '');
+
+  if (ret[i].status !== 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`);
+    logger.error(ret[i].reason);
+    continue;
   }
-  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
 }
-Init.initConfig()
-Init.initClient()
-export { apps }
-logger.info('---------------')
+
+logger.info(logger.green("- MJ-PLUGIN 载入成功"));
+logger.info(logger.magenta(`- 欢迎加入新组织【貓娘樂園🍥🏳️‍⚧️】（群号 551081559）`));
+
+export { apps };
